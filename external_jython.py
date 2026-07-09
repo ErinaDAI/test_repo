9397542f21f958a2ffc461cf5a47656f17da91f7
ErinaDAI <erina.smiles@digital.ai>
2026-07-09 10:21:42 +0530

    Add files via upload

---
+++ external_jython.py
@@ -0,0 +1,12 @@
+from java.lang import System
+
+print("External Jython Script")
+
+print("Release Title:")
+print(release.title)
+
+print("Task Title:")
+print(task.title)
+
+print("Java Version:")
+print(System.getProperty("java.version"))
